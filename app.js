ecf3fcbc54441fdd5fd8f819f2fbf8e48e202f79
vaibhav thakur <overcastvaibhav@gmail.com>
2020-07-21 11:50:34 +0530

    Remove from basket button added

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 
 
 app.get("/",function(req,res){
-  res.render("home");
+   res.render("home");
 });
 
 app.post("/",function(req,res){
@@ -38,6 +38,7 @@ app.get("/basket",function(req,res){
 });
 
 let array=[];
+const length=array.length;
 
 app.post("/basket",function(req,res){
    const items={
@@ -46,13 +47,37 @@ app.post("/basket",function(req,res){
       rating:req.body.rating,
       img:req.body.img
     }
+    //item is pushed inside array
     array.push(items);
+    //grabbing the index of current item
+    let t=array.indexOf(items);
+    //adding new value index inside current item whicj=h is t
+    items.index = t;
+
     let sum=0;
+    //converting price which is string to int value and adding it to sum
     for (let i=0;i<array.length;i++){
       sum=parseInt(array[i].price)+sum;
     }
-   res.render("basket",{items:items,array:array,sum:sum});
+
+   res.render("basket",{array:array,sum:sum});
 });
+
+
+app.post("/delete",function(req,res){
+  //Grabbing index of array passed through remove from basket button and deleting it from current array
+  const t = req.body.button;
+  if (t > -1) {
+   array.splice(t, 1);
+   }
+  let sum=0;
+  for (let i=0;i<array.length;i++){
+   sum=parseInt(array[i].price)+sum;
+ }
+ res.render("basket",{array:array,sum:sum});
+});
+
+
 
 let port = process.env.PORT;
 if (port == null || port == "") {
@@ -61,4 +86,5 @@ if (port == null || port == "") {
 app.listen(port,function(){
   console.log("server started on port 3000");
 });
-//https://images-eu.ssl-images-amazon.com/images/G/31/AmazonVideo/2020/X-site/SingleTitle/BITS/Launch/3000x1200_Hero-Tall_np._CB410403189_.jpg
+//
+//background-blue=https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg
