75c488d370055af7d407583524c3184f5d6836cd
vaibhav thakur <overcastvaibhav@gmail.com>
2020-07-22 19:28:25 +0530

    MongoDb Add

--- app.js
+++ app.js
@@ -2,12 +2,23 @@ const express=require("express");
 const ejs=require("ejs");
 const bodyParser=require("body-parser");
 const app=express();
+const mongoose=require("mongoose");
+mongoose.connect("mongodb://localhost:27017/Amazondb", {useUnifiedTopology: true,useNewUrlParser:true});
 
 app.set('view engine','ejs');
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
+
+const AmazonSchema=new mongoose.Schema({
+  p:String,
+  price:String,
+  rating:String,
+  img:String
+});
+
+const Amazon = mongoose.model("Amazon",AmazonSchema);
 
 app.get("/",function(req,res){
    res.render("home");
@@ -30,53 +41,44 @@ app.get("/tryprime",function(req,res){
 });
 
 app.get("/basket",function(req,res){
-  let sum=0;
-  for (let i=0;i<array.length;i++){
-    sum=parseInt(array[i].price)+sum;
-  }
-  res.render("basket",{array:array,sum:sum});
+  Amazon.find({}, function(err, item){
+    let sum=0;
+    for(let i=0;i<item.length;i++)
+    {
+      sum=parseInt(item[i].price)+sum;
+    }
+    res.render("basket", {
+      item:item,sum:sum
+      });
+  });
 });
 
-let array=[];
-const length=array.length;
 
 app.post("/basket",function(req,res){
-   const items={
+   const items=new Amazon({
       p:req.body.p,
       price:req.body.price,
       rating:req.body.rating,
-      img:req.body.img
-    }
-    //item is pushed inside array
-    array.push(items);
-    //grabbing the index of current item
-    let t=array.indexOf(items);
-    //adding new value index inside current item whicj=h is t
-    items.index = t;
-
-    let sum=0;
-    //converting price which is string to int value and adding it to sum
-    for (let i=0;i<array.length;i++){
-      sum=parseInt(array[i].price)+sum;
-    }
-
-   res.render("basket",{array:array,sum:sum});
+      img:req.body.img,
+    });
+    items.save(function(err){
+    if (!err){
+     res.redirect("/basket");
+     }
+     });
 });
 
 
 app.post("/delete",function(req,res){
-  //Grabbing index of array passed through remove from basket button and deleting it from current array
-  const t = req.body.button;
-  if (t > -1) {
-   array.splice(t, 1);
-   }
-  let sum=0;
-  for (let i=0;i<array.length;i++){
-   sum=parseInt(array[i].price)+sum;
- }
- res.render("basket",{array:array,sum:sum});
-});
-
+ const check=req.body.button;
+  Amazon.findByIdAndRemove(check,function(err){
+    if(!err)
+    {
+      console.log("success");
+      res.redirect("/basket");
+    }
+  });
+ });
 
 
 let port = process.env.PORT;
@@ -86,5 +88,17 @@ if (port == null || port == "") {
 app.listen(port,function(){
   console.log("server started on port 3000");
 });
-//
-//background-blue=https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg
+
+/*
+ <%=array.length%>
+ $<%=sum%>
+*/
+
+/*   let t = req.body.button;
+  if (t >-1) {
+   array.splice(t, 1);
+ }*/
+
+ /*  let t=array.indexOf(items);
+   items.index = t;
+   */
